01b0c90f37ee2311a06b4a1ccb41c0bb21e8ff0e
meheraj786 <meherajhosen786@gmail.com>
2026-03-22 16:16:14 +0600

    typewriter add

--- components/appComponents/Typewriter.tsx
+++ components/appComponents/Typewriter.tsx
@@ -1,16 +1,17 @@
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
 
 <Typewriter
-  onInit={(typewriter) => {
-    typewriter.typeString('Hello World!')
-      .callFunction(() => {
-        console.log('String typed out!');
-      })
-      .pauseFor(2500)
-      .deleteAll()
-      .callFunction(() => {
-        console.log('All strings were deleted');
-      })
-      .start();
-  }}
-/>
+	onInit={(typewriter) => {
+		typewriter
+			.typeString("Hello World!")
+			.callFunction(() => {
+				console.log("String typed out!");
+			})
+			.pauseFor(2500)
+			.deleteAll()
+			.callFunction(() => {
+				console.log("All strings were deleted");
+			})
+			.start();
+	}}
+/>;
