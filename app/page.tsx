a92f1052bab1a1d78bad47bb3b62368f20caaeef
meheraj786 <meherajhosen786@gmail.com>
2026-03-23 12:18:28 +0600

    homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { BlogsSection } from "@/components/appComponents/Blogs";
 import Categories from "@/components/appComponents/Categories";
+import { Doctors } from "@/components/appComponents/Doctor";
+import { FreelancersSection } from "@/components/appComponents/Freelancers";
 import Hero from "@/components/appComponents/Hero";
 import HospitalsAndRestaurants from "@/components/appComponents/HospitalsAndResturents";
+import { ShoppingMallsSection } from "@/components/appComponents/Malls";
+import { PCBuilderSection } from "@/components/appComponents/PcBuilder";
+import { PetCareSection } from "@/components/appComponents/PetCare";
+import { RentalsSection } from "@/components/appComponents/Rental";
 import TravelPlaces from "@/components/appComponents/TravelPlaces";
+import { TutorsSection } from "@/components/appComponents/Tutors";
 
 export default function Home() {
 	return (
@@ -10,6 +18,14 @@ export default function Home() {
 			<Categories />
 			<TravelPlaces />
 			<HospitalsAndRestaurants />
+			<Doctors />
+			<FreelancersSection/>
+			<ShoppingMallsSection/>
+			<PCBuilderSection/>
+			<PetCareSection/>
+			<RentalsSection/>
+			<TutorsSection/>
+			<BlogsSection/>
 		</>
 	);
 }
