b210b66b87fc4e0356d27df7ef63b665c7ed8b4c
meheraj786 <meherajhosen786@gmail.com>
2026-03-27 18:13:13 +0600

    analytics add

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -3,68 +3,70 @@ import { Inter, Poppins } from "next/font/google";
 import "@/app/globals.css";
 import Footer from "@/components/appComponents/Footer";
 import Header from "@/components/appComponents/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
-	variable: "--font-inter",
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700"],
-	preload: true,
-	display: "swap",
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  preload: true,
+  display: "swap",
 });
 
 const poppins = Poppins({
-	variable: "--font-poppins",
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700"],
-	preload: true,
-	display: "swap",
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  preload: true,
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-	title: "About Dhaka",
-	description: "Explore the best of Dhaka City.",
-	viewport: "width=device-width, initial-scale=1.0",
-	icons: {
-		icon: "/favicon.ico",
-		apple: "/apple-touch-icon.png",
-	},
-	manifest: "/site.webmanifest",
-	themeColor: "#f3f4f6",
-	openGraph: {
-		title: "About Dhaka",
-		description: "Explore the best of Dhaka City.",
-		url: "https://aboutdhaka.vercel.app/",
-		siteName: "About Dhaka",
-		images: [
-			{
-				url: "https://aboutdhaka.vercel.app/og.png",
-				width: 800,
-				height: 600,
-			},
-		],
-		locale: "en_US",
-		type: "website",
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "About Dhaka",
-		description: "Explore the best of Dhaka City.",
-		images: ["https://aboutdhaka.vercel.app/og.png"],
-	},
+  title: "About Dhaka",
+  description: "Explore the best of Dhaka City.",
+  viewport: "width=device-width, initial-scale=1.0",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
+  themeColor: "#f3f4f6",
+  openGraph: {
+    title: "About Dhaka",
+    description: "Explore the best of Dhaka City.",
+    url: "https://aboutdhaka.vercel.app/",
+    siteName: "About Dhaka",
+    images: [
+      {
+        url: "https://aboutdhaka.vercel.app/og.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "About Dhaka",
+    description: "Explore the best of Dhaka City.",
+    images: ["https://aboutdhaka.vercel.app/og.png"],
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={`${inter.variable} ${poppins.variable} antialiased`}>
-				<Header />
-				<div className="pt-16">{children}</div>
-				<Footer />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${poppins.variable} antialiased`}>
+        <Header />
+        <div className="pt-16">{children}</div>
+        <Footer />
+        <Analytics />
+      </body>
+    </html>
+  );
 }
