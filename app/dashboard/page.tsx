e9f85b4966d5aac45b4c1aa74d0eeaaa5e264b81
meheraj786 <meherajhosen786@gmail.com>
2026-04-19 01:38:50 +0600

    minor

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 
 const page = () => {
+	const _num = 0;
 	return (
 		<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
 			<SectionCards />
