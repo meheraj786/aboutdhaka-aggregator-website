c56352296372b27cea7c54ee12abeb780f4894a8
meheraj786 <meherajhosen786@gmail.com>
2026-07-27 22:05:22 +0600

    docker file add

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**",
-			},
-			{
-				protocol: "http",
-				hostname: "**",
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
